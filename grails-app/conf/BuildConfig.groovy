3d853a2a527641220a28d626df726dabd91e8c5f
Eduardo Díaz <iamedu@gmail.com>
2014-07-20 17:46:24 -0500

    Uploading to bintray

--- grails-app/conf/BuildConfig.groovy
+++ grails-app/conf/BuildConfig.groovy
@@ -46,7 +46,8 @@ grails.project.dependency.resolution = {
     }
 
     plugins {
-        build(":release:3.0.1",
+        build(":bintray-upload:0.2",
+              ":release:3.0.1",
               ":rest-client-builder:1.0.3") {
             export = false
         }
