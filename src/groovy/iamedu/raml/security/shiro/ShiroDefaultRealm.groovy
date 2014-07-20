1489de5aa5b36507561a03afa3dd54ef4e0c476b
Eduardo Díaz <iamedu@gmail.com>
2014-07-20 18:08:59 -0500

    Permissions

--- src/groovy/iamedu/raml/security/shiro/ShiroDefaultRealm.groovy
+++ src/groovy/iamedu/raml/security/shiro/ShiroDefaultRealm.groovy
@@ -21,6 +21,14 @@ class ShiroDefaultRealm extends AuthorizingRealm {
     String className = config.iamedu.raml.security.shiro.userClass
     String usernameField = config.iamedu.raml.security.shiro.usernameField
     String passwordField = config.iamedu.raml.security.shiro.passwordField
+    if(!usernameField) {
+      usernameField = "username"
+    }
+
+    if(!passwordField) {
+      passwordField = "password"
+    }
+
     def User = grailsApplication.getClassForName(className)
 
     def user = User.findWhere((usernameField): token.username)
@@ -42,7 +50,27 @@ class ShiroDefaultRealm extends AuthorizingRealm {
     String permissionsField = config.iamedu.raml.security.shiro.permissionsField
     String rolesField = config.iamedu.raml.security.shiro.rolesField
 
+    if(!usernameField) {
+      usernameField = "username"
+    }
+
+    if(!passwordField) {
+      passwordField = "password"
+    }
+
+    if(!permissionsField) {
+      permissionsField = "permissions"
+    }
+
+    if(!rolesField) {
+      rolesField = "roles"
+    }
+
     String rolePermissionField = config.iamedu.raml.security.shiro.rolePermissionField
+
+    if(!rolePermissionField) {
+      rolePermissionField = "permissions"
+    }
 
     def User = grailsApplication.getClassForName(className)
 
